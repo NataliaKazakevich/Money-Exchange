1098fdb7f57c5b01019ac16ff3b8c45a3059265d
Natalia Kazakevich <Natallia_Kazakevich@mail.ru>
2018-09-30 13:04:05 +0300

    task 5

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-// PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+     module.exports = function makeExchange(currency) {
+        function div(val, by){
+          return (val - val % by) / by;
+        } 
+        const H = 50, Q = 25, D = 10, N = 5, P = 1;
+        let currencyLocal, tmp;
+        amountCoins = {};
+        errReport = {error: "You are rich, my friend! We don't have so much coins for exchange"};
+        currencyLocal = currency;
+        if (currencyLocal > 10000 ) {
+            return amountCoins.err = errReport;
+        }
+        tmp = div(currencyLocal, H);
+        if (tmp >= 1) amountCoins.H = tmp;
+        currencyLocal -= H * tmp;
+      
+        tmp = div(currencyLocal, Q);
+        if (tmp >= 1) amountCoins.Q = tmp;
+        currencyLocal -= Q * tmp;
+      
+        tmp = div(currencyLocal, D);
+        if (tmp >= 1) amountCoins.D = tmp;
+        currencyLocal -= D * tmp;
+      
+        tmp = div(currencyLocal, N);
+        if (tmp >= 1) amountCoins.N = tmp;
+        currencyLocal -= N * tmp;
+      
+        tmp = div(currencyLocal, P);
+        if (tmp >= 1) amountCoins.P = tmp;
+        currencyLocal -= P * tmp;
+        return amountCoins;   
+  }
